760eb7f5e9f14012c9c5660d2e80ddcf6953d515
Chris Honeycutt <clhoneycutt83@gmail.com>
2019-07-09 09:34:18 -0400

    Navbar complete - updated gitignore

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import {NavMenu} from './components/NavMenu.js';
+import { NavMenu } from './components/NavMenu.js';
 
 export default class App extends Component {
   displayName = App.name
 
+  state = {
+    socialMediaLinks : [
+      { name: 'Facebook', url: 'https://www.facebook.com/clhoneycutt', id: 1 },
+      { name: 'Twitter', url: 'https://www.twitter.com/clhoneycutt', id: 2 },
+      { name: 'Instagram', url: 'https://www.instagram.com/clhone', id: 3 },
+    ]
+  }
+
   render() {
     return (
-      <div class="container">
-        <h1 class="display-1">Bloggster</h1>
-        <NavMenu />
+      <div>
+        <div className="container">
+          <h1 className="display-1">Bloggster</h1>
+        </div>
+        <NavMenu socialMediaLinks={this.state.socialMediaLinks} />
       </div>
     );
   }
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,16 +1,60 @@
 ﻿import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 // import './NavMenu.css';
 
 export class NavMenu extends Component {
   displayName = NavMenu.name
-
+    
+  state = {
+    searchTerm: ''
+  }
+  
+  handleChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+  search = (e) => {
+    e.preventDefault();
+    console.log(this.state.searchTerm)
+  }
+  
   render() {
+   
+    const { socialMediaLinks } = this.props;
+    const linksList = socialMediaLinks.map(socialMediaLink => {
+      return (
+        <li className="nav-item" key={socialMediaLink.id}>
+          <a className="nav-link" href={ socialMediaLink.url }>{ socialMediaLink.name }</a>
+        </li>
+      )
+    })
+
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="">Contact</a>
+              </li>
+              { linksList }
+            </ul>
+            <form className="form-inline my-2 my-lg-0" onSubmit= {this.search} >
+              <input className="form-control mr-sm-2 col-8" type="text" placeholder="Search" onChange={this.handleChange}/>
+              <button className="btn btn-outline-success m-2 my-sm-0 text">Search</button>
+            </form>
+
+          </div>
+        </div>
       </nav>
     );
   }
